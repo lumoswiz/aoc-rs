85351cde2e0e34b4b62e2789afadefb713f177a2
Nicholas Lordello <nlordell@gmail.com>
2018-12-06 21:34:18 +0100

    significant speedup

--- src/y2018/d05.rs
+++ src/y2018/d05.rs
@@ -1,28 +1,59 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
+use std::iter;
 
 pub fn puzzle1(input: &str) -> usize {
-    let mut chain = input.trim().to_string();
+    let chain = input.trim().as_bytes();
+    let mut reductions = BTreeSet::new();
+    let mut new_reductions = Vec::new();
 
     loop {
-        let mut reduction = None;
-        for ((i, c1), c2) in chain.chars().enumerate().zip(chain[1..].chars()) {
-            if c1.is_ascii_uppercase() ^ c2.is_ascii_uppercase()
-                && c1.to_ascii_uppercase() == c2.to_ascii_uppercase()
-            {
-                reduction = Some(i);
-                break;
+        {
+            let mut start = 0;
+            let mut indices = reductions
+                .iter()
+                .cloned()
+                .chain(iter::once(chain.len()))
+                .map(|i| {
+                    if i > start {
+                        let s = start;
+                        start = i + 2;
+                        (s..i)
+                    } else {
+                        start += 2;
+                        (0..0)
+                    }
+                })
+                .flatten();
+
+            let mut i1 = match indices.next() {
+                Some(i) => i,
+                None => return 0,
+            };
+            while let Some(i2) = indices.next() {
+                i1 = if chain[i1].is_ascii_uppercase() ^ chain[i2].is_ascii_uppercase()
+                    && chain[i1].to_ascii_uppercase() == chain[i2].to_ascii_uppercase()
+                {
+                    new_reductions.push(i1);
+                    match indices.next() {
+                        Some(i) => i,
+                        None => break,
+                    }
+                } else {
+                    i2
+                };
             }
         }
 
-        match reduction {
-            Some(i) => {
-                chain.drain(i..=i + 1);
-            }
-            None => break,
+        if new_reductions.is_empty() {
+            break;
+        }
+
+        for reduction in new_reductions.drain(..) {
+            reductions.insert(reduction);
         }
     }
 
-    chain.len()
+    chain.len() - reductions.len() * 2
 }
 
 pub fn puzzle2(input: &str) -> i64 {
