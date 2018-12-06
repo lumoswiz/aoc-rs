b1830fff1efb135d5f2a28da593ea5415cec5d7c
Nicholas Lordello <nlordell@gmail.com>
2018-12-06 22:09:55 +0100

    slow working solution for second puzzle

--- src/y2018/d05.rs
+++ src/y2018/d05.rs
@@ -1,25 +1,26 @@
-use std::collections::BTreeSet;
+use std::cmp;
+use std::collections::{BTreeMap, HashSet};
 use std::iter;
 
-pub fn puzzle1(input: &str) -> usize {
+fn reduce_chain<I: Iterator<Item = usize>>(input: &str, skip: I) -> usize {
     let chain = input.trim().as_bytes();
-    let mut reductions = BTreeSet::new();
-    let mut new_reductions = Vec::new();
+    let mut reductions = skip.map(|s| (s, 1usize)).collect::<BTreeMap<_, _>>();
+    let mut pairs = Vec::new();
 
     loop {
         {
             let mut start = 0;
             let mut indices = reductions
                 .iter()
-                .cloned()
-                .chain(iter::once(chain.len()))
-                .map(|i| {
+                .map(|(i, l)| (*i, *l))
+                .chain(iter::once((chain.len(), 0)))
+                .map(|(i, l)| {
                     if i > start {
                         let s = start;
-                        start = i + 2;
+                        start = i + l;
                         (s..i)
                     } else {
-                        start += 2;
+                        start += l;
                         (0..0)
                     }
                 })
@@ -30,10 +31,10 @@ pub fn puzzle1(input: &str) -> usize {
                 None => return 0,
             };
             while let Some(i2) = indices.next() {
-                i1 = if chain[i1].is_ascii_uppercase() ^ chain[i2].is_ascii_uppercase()
-                    && chain[i1].to_ascii_uppercase() == chain[i2].to_ascii_uppercase()
+                i1 = if chain[i1].is_ascii_lowercase() ^ chain[i2].is_ascii_lowercase()
+                    && chain[i1].to_ascii_lowercase() == chain[i2].to_ascii_lowercase()
                 {
-                    new_reductions.push(i1);
+                    pairs.push(i1);
                     match indices.next() {
                         Some(i) => i,
                         None => break,
@@ -44,20 +45,43 @@ pub fn puzzle1(input: &str) -> usize {
             }
         }
 
-        if new_reductions.is_empty() {
+        if pairs.is_empty() {
             break;
         }
 
-        for reduction in new_reductions.drain(..) {
-            reductions.insert(reduction);
+        for i in pairs.drain(..) {
+            reductions.insert(i, 2);
         }
     }
 
-    chain.len() - reductions.len() * 2
+    let total_reductions: usize = reductions.values().cloned().sum();
+    chain.len() - total_reductions
 }
 
-pub fn puzzle2(input: &str) -> i64 {
-    0
+pub fn puzzle1(input: &str) -> usize {
+    reduce_chain(input, iter::empty())
+}
+
+pub fn puzzle2(input: &str) -> usize {
+    let chain = input.trim().as_bytes();
+    let unique_units = chain
+        .iter()
+        .map(|b| b.to_ascii_lowercase())
+        .collect::<HashSet<_>>();
+
+    let mut min = None;
+    for unit in unique_units {
+        let skip = chain
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| c.to_ascii_lowercase() == unit)
+            .map(|(i, _)| i);
+        let reduced_len = reduce_chain(input, skip);
+
+        min = min.map(|m| cmp::min(m, reduced_len)).or(Some(reduced_len));
+    }
+
+    min.unwrap_or(chain.len())
 }
 
 #[cfg(test)]
@@ -69,6 +93,6 @@ mod tests {
 
     #[test]
     fn puzzle2() {
-        assert_eq!(super::puzzle2(""), 0);
+        assert_eq!(super::puzzle2("dabAcCaCBAcCcaDA"), 4);
     }
 }
