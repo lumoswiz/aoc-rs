7f30573df2802c6008deaca927d06cb6051fed48
Nicholas Lordello <nlordell@gmail.com>
2018-12-19 15:27:10 +0100

    restart day 15

--- src/year2018/day15.rs
+++ src/year2018/day15.rs
@@ -1,55 +1,8 @@
-#![allow(bad_style, unused_variables, dead_code)]
-
-use crate::util::Grid;
-use nalgebra::Point2;
-
-enum UnitKind {
-    Elf,
-    Goblin,
-}
-
-struct Unit {
-    kind: UnitKind,
-    pos: Point2<usize>,
-}
-
-struct Area {
-    grid: Grid,
-    units: Vec<Unit>,
-}
-
-impl Area {
-    fn new(layout: &str) -> Area {
-        let mut grid = Grid::from_layout(layout);
-        let mut units = Vec::new();
-
-        for (pos, a) in grid.iter_mut() {
-            let kind = match *a {
-                b'E' => Some(UnitKind::Elf),
-                b'G' => Some(UnitKind::Goblin),
-                _ => None,
-            };
-            if let Some(kind) = kind {
-                *a = b'.';
-                units.push(Unit { kind, pos });
-            }
-        }
-
-        Area { grid, units }
-    }
-
-    fn sort_units(&mut self) {
-        self.units.sort_unstable_by_key(|u| (u.pos[1], u.pos[0]));
-    }
-
-
-}
-
-pub fn puzzle1(input: &str) -> i64 {
+pub fn puzzle1(_input: &str) -> i64 {
     0
 }
 
-pub fn puzzle2(input: &str) -> i64 {
+pub fn puzzle2(_input: &str) -> i64 {
     0
 }
 
