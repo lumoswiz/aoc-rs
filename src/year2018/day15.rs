13c49a836bd8fde90c8d1fcd3c2a4ce1a72969be
Nicholas Lordello <nlordell@gmail.com>
2018-12-28 13:42:25 +0100

    progrssed day 15

--- src/year2018/day15.rs
+++ src/year2018/day15.rs
@@ -31,6 +31,8 @@ impl UnitKind {
 struct Unit {
     kind: UnitKind,
     pos: Point2<usize>,
+    attack: i32,
+    health: i32,
 }
 
 struct Battle {
@@ -39,6 +41,8 @@ struct Battle {
 
     dists: HashMap<Point2<usize>, usize>,
     dists_pending: Vec<Point2<usize>>,
+    poss: HashMap<Point2<usize>, usize>,
+    targets: Vec<(usize, usize)>,
 }
 
 impl Battle {
@@ -48,7 +52,12 @@ impl Battle {
 
         for (pos, a) in grid.iter_mut() {
             if let Some(kind) = UnitKind::from_raw(*a) {
-                units.push(Unit { kind, pos });
+                units.push(Unit {
+                    kind,
+                    pos,
+                    attack: 3,
+                    health: 200,
+                });
             }
         }
 
@@ -57,6 +66,8 @@ impl Battle {
             units,
             dists: HashMap::new(),
             dists_pending: Vec::new(),
+            poss: HashMap::new(),
+            targets: Vec::new(),
         }
     }
 
@@ -84,7 +95,7 @@ impl Battle {
             let dist = self.dists[&pos] + 1;
 
             for next_pos in util::adjacent4(pos) {
-                if self.grid.get(pos).is_none() {
+                if self.grid.get(next_pos) != Some(b'.') {
                     continue;
                 }
 
@@ -106,6 +117,19 @@ impl Battle {
         'units: for i in 0..self.units.len() {
             let unit = self.units[i].clone();
             self.calc_dists(unit.pos);
+
+            // let mut y = 0;
+            // for (p, _) in self.grid.iter() {
+            //     if p[1] != y {
+            //         println!();
+            //         y = p[1];
+            //     }
+            //     match self.dists.get(&p) {
+            //         Some(d) if *d != usize::MAX => print!("{}", d),
+            //         _ => print!("#"),
+            //     };
+            // }
+            // println!();
 
             let enemies = self.units.iter().filter(|u| u.kind != unit.kind);
             let targets = enemies.map(|u| util::adjacent4(u.pos)).flatten();
@@ -140,33 +164,99 @@ impl Battle {
             self.units[i].pos = next_pos;
             self.grid[unit.pos] = b'.';
             self.grid[next_pos] = unit.kind.into_raw();
+        }
 
+        if movement {
             println!("MOVE:\n{:?}", self.grid);
         }
 
         movement
     }
 
-    fn attack(&mut self, moved: bool) {
-        for i in 0..self.units.len() {
-            let kind = self.units[i].kind;
+    fn store_poss(&mut self) {
+        self.poss.clear();
+        for (i, unit) in self.units.iter().enumerate() {
+            self.poss.insert(unit.pos, i);
         }
     }
 
-    fn outcome(&mut self) {
-        // while !self.complete() {
-        for _ in 0..5 {
-            let moved = self.movement();
-            self.attack(moved);
+    fn attack(&mut self, moved: bool) -> usize {
+        self.store_poss();
+
+        self.targets.clear();
+        for i in 0..self.units.len() {
+            let kind = self.units[i].kind;
+            let target = util::adjacent4(self.units[i].pos)
+                .filter_map(|p| self.poss.get(&p))
+                .filter(|i| self.units[**i].kind != kind)
+                .min_by_key(|i| {
+                    (
+                        self.units[**i].health,
+                        self.units[**i].pos[1],
+                        self.units[**i].pos[0],
+                    )
+                });
+
+            if let Some(target) = target {
+                self.targets.push((*target, i));
+            }
         }
+
+        println!("{:?}", self.targets);
+        let mut rounds = 0;
+        loop {
+            rounds += 1;
+            let mut died = false;
+            for (target, attacker) in self.targets.iter().cloned() {
+                if self.units[attacker].health <= 0 {
+                    continue;
+                }
+                self.units[target].health -= self.units[attacker].attack;
+                if self.units[target].health <= 0 {
+                    died = true;
+                    self.grid[self.units[target].pos] = b'.';
+                }
+            }
+
+            if died {
+                self.units.retain(|u| u.health > 0);
+                println!("ATTACK:\n{:?}", self.grid);
+            }
+
+            if moved || died {
+                break;
+            }
+        }
+
+        for u in self.units.iter() {
+            match u.kind {
+                UnitKind::Elf => println!("E({}) @{},{}", u.health, u.pos[0], u.pos[1]),
+                UnitKind::Goblin => println!("G({}) @{},{}", u.health, u.pos[0], u.pos[1]),
+            }
+        }
+
+        rounds
+    }
+
+    fn outcome(&mut self) -> usize {
+        // for _ in 0..9 {
+        let mut rounds = 0;
+        while !self.complete() {
+            self.sort_units();
+            let moved = self.movement();
+            rounds += self.attack(moved);
+
+            println!("----- {} -----", rounds);
+        }
+
+        let total_health: i32 = self.units.iter().map(|u| u.health).sum();
+        rounds * (total_health as usize)
     }
 }
 
-pub fn puzzle1(input: &str) -> i64 {
+pub fn puzzle1(input: &str) -> usize {
     let mut battle = Battle::new(input);
-    battle.outcome();
-
-    0
+    battle.outcome()
 }
 
 pub fn puzzle2(_input: &str) -> i64 {
@@ -189,7 +279,84 @@ mod tests {
                     #######
                 "
             ),
-            47
+            27730
+        );
+
+        assert_eq!(
+            super::puzzle1(
+                r"
+                    #######
+                    #G..#E#
+                    #E#E.E#
+                    #G.##.#
+                    #...#E#
+                    #...E.#
+                    #######
+                "
+            ),
+            36334
+        );
+
+        assert_eq!(
+            super::puzzle1(
+                r"
+                    #######
+                    #E..EG#
+                    #.#G.E#
+                    #E.##E#
+                    #G..#.#
+                    #..E#.#
+                    #######
+                "
+            ),
+            39514
+        );
+
+        assert_eq!(
+            super::puzzle1(
+                r"
+                    #######
+                    #E.G#.#
+                    #.#G..#
+                    #G.#.G#
+                    #G..#.#
+                    #...E.#
+                    #######
+                "
+            ),
+            27755
+        );
+
+        assert_eq!(
+            super::puzzle1(
+                r"
+                    #######
+                    #.E...#
+                    #.#..G#
+                    #.###.#
+                    #E#G#G#
+                    #...#G#
+                    #######
+                "
+            ),
+            28944
+        );
+
+        assert_eq!(
+            super::puzzle1(
+                r"
+                    #########
+                    #G......#
+                    #.E.#...#
+                    #..##..G#
+                    #...##..#
+                    #...#...#
+                    #.G...G.#
+                    #.....G.#
+                    #########
+                "
+            ),
+            18740
         );
     }
 
