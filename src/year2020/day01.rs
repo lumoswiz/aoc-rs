fd4ca8e0a70d4a6c745b338f69113eae02fc3b52
Ben Smith <bh2smith@gmail.com>
2021-01-13 12:30:24 +0100

    finish 2020-01

--- src/year2020/day01.rs
+++ src/year2020/day01.rs
@@ -1,10 +1,11 @@
 use crate::util;
 use itertools::Itertools;
 
-pub fn solve(numbers: Vec<i32>, size: u8) -> i32 {
-    for combo in numbers.iter().combinations(2) {
-        if combo.iter().sum() == 2020 {
-            return combo.iter().product();
+pub fn solve(numbers: Vec<i32>, size: usize) -> i32 {
+    for combo in numbers.iter().combinations(size) {
+        let s: i32 = combo.iter().map(|x| **x).sum();
+        if s == 2020 {
+            return combo.iter().map(|x| **x).product();
         }
     }
     0
@@ -17,7 +18,7 @@ pub fn puzzle1(input: &str) -> i32 {
     solve(parsed_input, 2)
 }
 
-pub fn puzzle2(_input: &str) -> i32 {
+pub fn puzzle2(input: &str) -> i32 {
     let parsed_input = util::split(input)
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
@@ -40,6 +41,21 @@ mod tests {
                 "
             ),
             514579
+        );
+    }
+    #[test]
+    fn puzzle2() {
+        assert_eq!(
+            super::puzzle2(
+                r"1721
+                        979
+                        366
+                        299
+                        675
+                        1456
+                "
+            ),
+            241861950
         );
     }
 }
